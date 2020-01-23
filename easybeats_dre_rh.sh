28ebedc4790fc426313576be6921527f25c5b9c0
acseazzu <acseazzu@users.noreply.github.com>
2020-01-23 13:06:55 -0700

    Update easybeats_dre_rh.sh

--- easybeats_dre_rh.sh
+++ easybeats_dre_rh.sh
@@ -14,7 +14,7 @@ systemctl enable filebeat
 
 echo "$(tput setaf 6) ---- Installing Metricbeat ----$(tput sgr0)"
 curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.5.2-x86_64.rpm
-dpkg -i metricbeat-7.5.2-x86_64.rpm
+rpm -i metricbeat-7.5.2-x86_64.rpm
 metricbeat modules enable system
 echo "$(tput setaf 6) ---- Starting Metricbeat ----$(tput sgr0)"
 systemctl enable metricbeat
@@ -23,14 +23,14 @@ systemctl enable metricbeat
 echo "$(tput setaf 6) ---- Installing Packetbeat ----$(tput sgr0)"
 apt-get install libpcap0.8
 curl -L -O https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-7.5.2-x86_64.rpm
-dpkg -i packetbeat-7.5.2-x86_64.rpm
+rpm -i packetbeat-7.5.2-x86_64.rpm
 echo "$(tput setaf 6) ---- Starting Packetbeat ----$(tput sgr0)"
 systemctl enable packetbeat
 
 
 echo "$(tput setaf 6) ---- Installing Auditbeat ----$(tput sgr0)"
 curl -L -O https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.5.2-x86_64.rpm
-dpkg -i auditbeat-7.5.2-x86_64.rpm
+rpm -i auditbeat-7.5.2-x86_64.rpm
 echo "$(tput setaf 6) ---- Starting Auditbeat ----$(tput sgr0)"
 systemctl enable auditbeat
  
